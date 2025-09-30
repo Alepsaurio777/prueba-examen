25967ccc0861979e89c45b719828856f1b5c58c8
Roman <l23920255@voaxaca.tecnm.mx>
2025-09-29 21:27:34 -0600

    primer commit de prueba

---
+++ src/roman.js
@@ -0,0 +1,11 @@
+// Saludo personalizado usando prompt y console.log
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('¿Cuál es tu nombre? ', nombre => {
+    console.log(`Hola, ${nombre}! 👋`);
+    readline.close();
+});
+// primeras lineas de codigo 
